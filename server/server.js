917c7532cbeb5b37c77d54b0afda6edbba452190
thiendao <thiendaocong@gmail.com>
2025-07-01 16:45:47 +0700

    nhánh mới

--- server/server.js
+++ server/server.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const connectDB = require("./config/db");
+
 const corsOptions = {
   origin: "http://localhost:5173",
 };
 
+// Connect to MongoDB
+connectDB();
+
 app.use(cors(corsOptions));
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ fruits: ["apple", "banana", "cherry"]});
+  res.json({ fruits: ["apple", "banana", "cherry"] });
 });
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
-
